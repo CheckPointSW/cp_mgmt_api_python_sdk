cbecca8ec48ea72fcc62a32b786e0259092e089b
yaelg <yaelg@checkpoint.com>
2019-12-16 15:55:33 +0200

    Update init file to expose APIResponse object

--- cpapi/__init__.py
+++ cpapi/__init__.py
@@ -2,3 +2,4 @@ from .mgmt_api import APIClient
 from .mgmt_api import APIClientArgs
 from .api_exceptions import APIException
 from .api_exceptions import APIClientException
+from .api_response import APIResponse
