dfcbcffc69bc70f2fdd98fcd6292fa44bfa004b3
orso <orso@checkpoint.com>
2020-04-01 19:30:39 +0300

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="cpapi",
-    version="1.0.4",
+    version="1.1.0",
     author="API team",
     author_email="api_team@checkpoint.com",
     description="Check Point Management API SDK",
