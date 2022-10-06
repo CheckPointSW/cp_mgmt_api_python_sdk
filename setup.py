cb47ac6feb54993467858e7780747b0c31f2ad38
chkp-royl <royl@checkpoint.com>
2022-10-06 16:25:04 +0300

    handle broken pipe error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="cp-mgmt-api-sdk",
-    version="1.5.0",
+    version="1.6.0",
     author="API team",
     author_email="api_team@checkpoint.com",
     license='Apache 2.0',
